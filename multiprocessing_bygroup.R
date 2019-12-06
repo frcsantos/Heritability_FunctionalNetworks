4e3202db9a97d25e82019a0c2314775684be0d9d
frcsantos <fefelrcs@gmail.com>
2019-12-05 23:32:04 -0300

    group analysis

---
+++ multiprocessing_bygroup.R
@@ -0,0 +1,69 @@
+library("umx")
+library("tidyverse")
+library(parallel)
+library(MASS)
+library(foreach)
+library(doParallel)
+library(iterators)
+
+table<-read.csv("~/Fer/shen_268_parcellation_networklabels.csv", header=F)
+
+table$V2 <- as.factor(table$V2)
+tipos <- levels(table$V2)
+
+
+#Receiving processed table
+rois <- read_delim("final_table.txt", col_names=T, delim="\t")
+
+#Change sex to 0 and 1 ##
+rois <- mutate(rois, Gender = if_else(Gender=="M", 1, 0))
+
+
+#Multiply all by 100
+rois[,7:dim(rois)[2]] <-  rois[,7:dim(rois)[2]]*100
+        
+#Eliminating sex and age influence
+roinames <- colnames(rois[,7:((length(colnames(rois))/2)+3)])
+rois$age1 = rois$age2 = rois$Age
+rois$gender1 = rois$gender2 = rois$Gender
+
+#Getting parameters  
+for (l in tipos)
+{
+	roi_ind=c()
+	#print(i)
+	aux <- filter(table, V2==l)
+	for (j in (1:(dim(aux)[1]-1)))
+	{
+		for (k in ((j+1):dim(aux)[1]))
+		{
+		roi_ind <- c(roi_ind,paste("ROI",aux[j,1],aux[k,1],"T1", sep="_"))
+		}
+	}
+	A1=0.0
+	C1=0.0
+	E1=0.0
+	A=c()
+	C=c()
+	E=c()
+	n=5
+	nel=10
+	#registerDoParallel(5)
+	#system.time({
+	#r <- foreach (i, .combine=rbind) %dopar%{
+	for (i in 1:n){
+		ids <- sample(roi_ind, size=nel, replace=F)
+	        selDVs=gsub('.{1}$','',ids)
+	        tmp = umx_residualize(selDVs, "age", suffixes = 1:2, rois) ## Adapt script to sex and age
+	        roi_resid = suppressWarnings(umx_residualize(selDVs, "gender", suffixes = 1:2, tmp))
+	        mzData <- as.data.frame(roi_resid[roi_resid$Zigosity=="MZ",])
+	        dzData <- as.data.frame(roi_resid[roi_resid$Zigosity=="DZ",])
+	        m1 = suppressMessages(umxACEv(selDVs = selDVs, sep = "", dzData = dzData, mzData = mzData))
+		A1 = (A1+sum(diag(m1$output$algebras$top.A_std)))/nel
+		C1 = (C1+sum(diag(m1$output$algebras$top.C_std)))/nel
+		E1 = (E1+sum(diag(m1$output$algebras$top.E_std)))/nel
+		r <- cbind(A1,C1,E1)
+		print(l)
+		write.table(r, paste("multivariate_",l,sep=""), sep="\t", quote=F, append=T, col.names=F, row.names=F)
+	}
+} 
